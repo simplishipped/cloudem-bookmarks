38eee2801c07d4b9e3e82a550e0fc0bea36e21d7
joysauce <joysauce@sauce.com>
2024-07-11 20:26:38 -0400

    oolala

--- src/api/nftmarks-api.tsx
+++ src/api/nftmarks-api.tsx
@@ -1,23 +1,3 @@
-import { Nftmark } from "../types/types"
-import supabase from "./supabase";
-import axios from 'axios';
-
-export const mintCollectionToNft = async (nftmark: Nftmark) => {
-  const { data, error } = await supabase.from('nftmarks').insert(nftmark);
-  if (data) {
-    return data
-  } else {
-    return false;
-  }
-}
-
-export const getNftmarks = async () => {
-
-}
-
-export const getNftmarksByUser = async (userId: string) => {
-
-}
 
 
 // export default {
